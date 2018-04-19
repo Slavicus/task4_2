fa591ef48d215091ecebed094c2250ff940adc33
Slavicus <33653008+Slavicus@users.noreply.github.com>
2018-04-19 23:21:37 +0300

    Create ntp_verify-worked.sh

---
+++ ntp_verify-worked.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+systemctl is-active --quiet ntp
+if [ $? != 0 ]
+then
+echo "NOTICE: ntp is not running"
+systemctl start ntp
+fi
+
+cmp -s /etc/ntp.conf /etc/ntp.conf.bak
+if [ $? != 0 ]
+then
+echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
+diff -Nu /etc/ntp.conf /etc/ntp.conf.bak
+cp -f /etc/ntp.conf.bak /etc/ntp.conf
+systemctl restart ntp
+fi
